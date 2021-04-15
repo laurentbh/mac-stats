398a379316891242fd0602825b44428dc0707754
laurent buhler <lbuhler@gmail.com>
2021-04-15 09:36:49 -0700

    Implement recovery

--- main.go
+++ main.go
@@ -4,24 +4,18 @@ import (
 	"fmt"
 	"os"
 	"time"
-
-	"github.com/mitchellh/go-homedir"
 )
 
-var homeDir string
-
-func setup() {
-	homeDir, err := homedir.Dir()
-	if err != nil {
-		panic(err)
-	}
-
-}
 func main() {
+
+	recovery := NewRecovery()
+
+	activateRecovery := false
 
 	db, err := Connect()
 	if err != nil {
-		panic(err)
+		fmt.Printf("Recovery mode active (%v)\n", err)
+		activateRecovery = true
 	}
 	hostname, err := os.Hostname()
 	if err != nil {
@@ -29,15 +23,34 @@ func main() {
 	}
 
 	now := time.Now()
-	fmt.Printf("hostname: %s\nnow:%v", hostname, now)
+	fmt.Printf("hostname: %s\nnow:%v\n", hostname, now)
 	batteryInfo, err := Battery()
 	if err != nil {
 		panic(err)
 	}
+
 	fmt.Printf("battery: %v\n", *batteryInfo)
-	err = db.InsertBattery(hostname, now, *batteryInfo)
-	if err != nil {
-		fmt.Printf("error insert battery %v\n", err)
+	if activateRecovery {
+		recovery.SaveBattery(now, hostname, *batteryInfo)
+	} else {
+		recoveredBat, err := recovery.LoadBattery()
+		if err != nil {
+			panic(err)
+		}
+		for _, r := range recoveredBat {
+			err = db.InsertBattery(r.Host, r.Stamp, r.BatteryInfo)
+			if err != nil {
+				fmt.Printf("error insert battery %v\n", err)
+			} else {
+				if errDelete := os.Remove(r.FileName); errDelete != nil {
+					panic(errDelete)
+				}
+			}
+		}
+		err = db.InsertBattery(hostname, now, *batteryInfo)
+		if err != nil {
+			fmt.Printf("error insert battery %v\n", err)
+		}
 	}
 
 	now = time.Now()
@@ -45,10 +58,29 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("ssd: %v", *ssd)
-	err = db.InsertSSD(hostname, now, *ssd)
-	if err != nil {
-		fmt.Printf("error insert ssd %v\n", err)
+	fmt.Printf("ssd: %v\n", *ssd)
+
+	if activateRecovery {
+		recovery.SaveSsd(now, hostname, *ssd)
+	} else {
+		recoveredSsd, err := recovery.LoadSsd()
+		if err != nil {
+			panic(err)
+		}
+		for _, r := range recoveredSsd {
+			err = db.InsertSSD(r.Host, r.Stamp, r.SsdInfo)
+			if err != nil {
+				fmt.Printf("error insert ssd %v\n", err)
+			} else {
+				if errDelete := os.Remove(r.FileName); errDelete != nil {
+					panic(errDelete)
+				}
+			}
+		}
+		err = db.InsertSSD(hostname, now, *ssd)
+		if err != nil {
+			fmt.Printf("error insert ssd %v\n", err)
+		}
 	}
 
 	// err = PushCounter(hostname, *batteryInfo, *ssd)
